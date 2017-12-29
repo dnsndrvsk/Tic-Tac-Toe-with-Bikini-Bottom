ca7c5a1331e0702e27726d24f6fc17b5045b3ce7
Denis <dnsndrvsk@gmail.com>
2017-12-29 20:28:32 +0200

    Update game.js

--- public/js/game.js
+++ public/js/game.js
@@ -254,11 +254,9 @@ function handleClick(e) {
   }
 }
 
-
 for (var i = 0; i < tds.length; i++) {
   tds[i].addEventListener('click', handleClick);
 }
-
 
 helper.playMusic();
 helper.chooseDifficulty();
